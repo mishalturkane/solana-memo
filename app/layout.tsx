cf632f31222ddbcd97718ab877a630403d8e42a7
mishalturkane <mishalturkane@gmail.com>
2025-12-09 08:12:42 +0530

    first init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
 import { SolanaProvider } from "@/components/solana-provider";
 import "./globals.css";
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"], // Opt: include full weight range
+});
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>
+      {/* Apply className from the font */}
+      <body className={spaceGrotesk.className}>
         <SolanaProvider>{children}</SolanaProvider>
       </body>
     </html>
